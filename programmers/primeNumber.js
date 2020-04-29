d93903b78c6b6254609ee0e211d36f5a542a8d43
최영훈 <c_yh0327@naver.com>
2020-04-29 17:33:26 +0900

    solve

---
+++ programmers/primeNumber.js
@@ -0,0 +1,20 @@
+function solution(numbers) {
+    var primeNumber = [];
+   //number로 만들수 있는 수들 찾기.
+
+    for(let i = 1; i < 9999999 ; i++){
+        let isPrime = true;
+        for(let j =2; j<i; j++){
+            if(i%j == 0){
+                isPrime = false;
+            }
+        }
+
+        if(isPrime){
+            primeNumber.push(i)
+        }
+    }
+    return primeNumber;
+}
+
+console.log(solution("1000"));
