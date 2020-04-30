e8a7c7e28cc4f37b2697fe0ce5453b404cf10f61
최영훈 <c_yh0327@naver.com>
2020-04-30 16:19:22 +0900

    sp

--- programmers/middle.js
+++ programmers/middle.js
@@ -1,7 +1,14 @@
-function solution(s) {
+function solution(str) {
     var answer = '';
-    let len = parseInt(s.length/2);
-    console.log(len);
+    let len = str.length;
+    let halfLen = parseInt(len/2);
+    answer = len%2==0 ? str.slice(halfLen-1,halfLen+1) : str.slice(halfLen,halfLen+1)
     return answer;
 }
-solution('hello');
+console.log(solution('abcdef'));
+var str = "abcdef";
+let strLen = parseInt(str.length/2);
+console.log(strLen);
+var str2 = str.slice(strLen-1,strLen+1);
+console.log("str2:"+str2)
+
