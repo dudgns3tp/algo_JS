b73f7362008dd74c0c4aeae9155b2df0e03f97d0
최영훈 <c_yh0327@naver.com>
2021-01-07 23:28:43 +0900

    heap
    1. Scoville.py 풀이 완료.
    - heapq 모듈을 이용해서 풀이

---
+++ python/programmers/heap/P-Scoville.py
@@ -0,0 +1,15 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+      if len(scoville) == 1:
+        answer = -1
+        break
+      mixed = heapq.heappop(scoville) + heapq.heappop(scoville)*2
+      heapq.heappush(scoville, mixed)
+      answer += 1
+    return answer
+
+print(solution([1, 2, 3, 9, 10, 12], 7))
--- python/programmers/heap/P-heap.py
+++ python/programmers/heap/P-heap.py
@@ -8,15 +8,16 @@ def solution(n, exec_times):
 
   FINISHED_AT, SERVER_ID, JOBS_DONE = 0, 1, 2
   servers= [[0, i, 0] for i in range(n)]
-  print(servers)
   heapq.heapify(servers)
   print(servers)
-  print(queue)
-  """
+
   while queue:
     target_server = heapq.heappop(servers)
-    target_server[FINISHED_AT] += queue.popleft()
-    target_sevrer[JOBS_DONE] += 1
-    print(servers)
-"""
+    target_server[FINISHED_AT] += queue.popleft() # [3,0,0], [0,1,0], [0,2,0]
+    target_server[JOBS_DONE] += 1
+    heapq.heappush(servers, target_server)
+
+  servers.sort(key=lambda x: x[SERVER_ID])
+  return[server[JOBS_DONE] for server in servers]
+
 print(solution(3, exec_times))
